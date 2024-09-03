189def5584d946b2b78b0d1dce66789f48714372
Philipp Haussleiter <philipp@haussleiter.de>
2024-09-03 16:22:42 +0200

    dedeployment cleanup

---
+++ scripts/prepare-docker-compose.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+cat << EOF
+version: "3"
+
+networks:
+  traefik_default:
+    external: true
+
+volumes:
+  config:
+  data:
+
+services:
+  deployment:
+    image: $APP_DEPLOY_IMAGE
+    container_name: $APP_DEPLOY_NAME
+      - data:/data
+    networks:
+      - traefik_default
+    labels:
+      - "traefik.enable=true"
+      - "traefik.http.routers.$APP_DEPLOY_NAME.rule=Host(\`$APP_URL\`)"
+      - "traefik.http.routers.$APP_DEPLOY_NAME.entrypoints=websecure"
+      - "traefik.http.routers.$APP_DEPLOY_NAME.tls=true"
+      - "traefik.http.routers.$APP_DEPLOY_NAME.tls.certresolver=leresolver"
+      - "traefik.http.services.$APP_DEPLOY_NAME.loadbalancer.server.port=$APP_PORT"
+    restart: unless-stopped
+    healthcheck:
+      ## In production the healthcheck section should be commented.
+      disable: true
+    environment:
+      - TZ=Europe/Berlin
+
+EOF
